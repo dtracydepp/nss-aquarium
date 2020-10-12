3d5a155a7f253d693067e176852552e91dce3d2d
dtracydepp <tracydepp82@gmail.com>
2020-10-12 15:55:44 -0500

    updated fish data in fish dataprovider.js

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -2,7 +2,7 @@ const fishCollection = [
     {
         name: "Scar",
         species: "Butterfly Fish",
-        length: "5-9 inches",
+        length: "10 inches",
         harvest_location: "Coral reefs in the Atlantic, Indian and Pacific Oceans",
         diet: "Zooplankton, tiny motile crustaceans and mollusks, and types of soft and stony corals",
         image: "https://www.fishkeepingworld.com/wp-content/uploads/2018/12/Threadfin-Butterflyfish.jpg"
@@ -11,7 +11,7 @@ const fishCollection = [
     {
         name: "Lilo",
         species: "Discus",
-        length: "5-6 inches",
+        length: "4 inches",
         harvest_location: "Amazon River Delta",
         diet: "Algae flakes, tropical flakes, and shrimp pellets.",
         image: "https://www.petlandtexas.com/wp-content/uploads/2020/01/Petland_Texas_Discus.jpg"
@@ -20,7 +20,7 @@ const fishCollection = [
     {
         name: "Stitch",
         species: "Flowerhorn Cichlid",
-        length: "16 inches",
+        length: "12 inches",
         harvest_location: "Singapore and Malaysia",
         diet: "Crickets, mealworms, and frozen shrimp.",
         image: "https://www.petlandtexas.com/wp-content/uploads/2020/01/Petland_Texas_Flowerhorn_Cichlid.jpg"
